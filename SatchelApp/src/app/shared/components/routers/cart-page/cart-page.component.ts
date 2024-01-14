1c4063540287528a1610b69a7a17aa0a485380e5
3xA3xA <drainedsimp@gmail.com>
2024-01-14 16:02:45 +0300

    cart-page data from back

--- SatchelApp/src/app/shared/components/routers/cart-page/cart-page.component.ts
+++ SatchelApp/src/app/shared/components/routers/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CartPageService } from 'src/app/core/services/cart-page.service';
+import { UserService } from 'src/app/core/services/user.service';
+import { Product } from '../catalog/catalog.component';
 
 @Component({
   selector: 'app-cart-page',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class CartPageComponent {
 
+  constructor(private cartPageService: CartPageService, private userService: UserService) { }
+
+  shoppingCart : Product[] = [];
+
+  ngOnInit() {
+    this.cartPageService.GetShoppingCart(this.userService.userId).subscribe(
+      (productsFromQuery: Product[]) => {
+        this.shoppingCart = productsFromQuery;
+      },
+      (error) => {
+        console.error('Error fetching products', error);
+      }
+    );
+  }
 }
--- SatchelApp/src/app/shared/components/routers/user-page/user-page.component.ts
+++ SatchelApp/src/app/shared/components/routers/user-page/user-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
 import { IUserPageData } from 'src/app/core/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-page',
@@ -8,7 +9,7 @@ import { IUserPageData } from 'src/app/core/services/user.service';
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent {
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   defaultUserPhoto: string = 'https://img.freepik.com/free-vector/illustration-businessman_53876-5856.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1705190400&semt=ais';
 
@@ -22,10 +23,11 @@ export class UserPageComponent {
   }
 
   ngOnInit() {
+    if (!this.userService.isAuthorized)
+      this.router.navigate(['/']);
     this.userService.getUserData().subscribe(
       (data: IUserPageData) => {
         this.userData = data;
-        console.log(this.userData);
         if (this.userData.userPhotoSrc == null)
           this.userData.userPhotoSrc = this.defaultUserPhoto;
         console.log(this.userData)
